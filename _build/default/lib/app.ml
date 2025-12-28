904bcddc4c80ca47195ffe9c76a2661a8e20d513
Reubotimus <reubencook555@gmail.com>
2025-12-28 16:30:35 +1100

    Add .gitignore

--- _build/default/lib/app.ml
+++ _build/default/lib/app.ml
@@ -1,2 +1,8 @@
 let handler =
-        Dream.router (List.concat [ Routes.Health.routes ])
+  Dream.router
+    (List.concat
+       [
+         Routes.Health.routes;
+         Routes.Pages.routes;
+         [ Dream.get "/static/**" (Dream.static "static") ];
+       ])
